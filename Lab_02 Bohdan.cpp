9176200f37de32c20fadfc3b406a0a8215f914e6
Bohdan Kyrychynskyi <bkyrychynskyi@gmail.com>
2020-10-16 23:27:34 +0300

    111

--- Lab_02 Bohdan.cpp
+++ Lab_02 Bohdan.cpp
@@ -16,12 +16,12 @@ int main()
 
 	cout << "a ="; cin >> a;
 	z1 = cos(a) + cos(2 * a) + cos(6 * a) + cos(7 * a);
-	z2 = 4 * cos(a / 2) * cos((5./ 2) * a) * cos(4 * a);
+	z2 = 4 * cos(a / 2) * cos(5./ 2 * a) * cos(4 * a);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 
 	
 	cin.get();
-	retu
+	return 0;
 }
