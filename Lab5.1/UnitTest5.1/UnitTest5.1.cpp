c6cf12101bd5e61bf54c884d1be7f035b4bfe68c
in7erlude <bkyrychynskyi@gmail.com>
2020-11-16 12:12:45 +0200

    change1

---
+++ Lab5.1/UnitTest5.1/UnitTest5.1.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.1/Lab5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51
+{
+	TEST_CLASS(UnitTest51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double l = h(1, 1);
+			Assert::AreEqual(l, 0.5);
+		}
+	};
+}
