3107d5cb21407ad9b757b7dc2ea7cd28bccc7b20
in7erlude <bkyrychynskyi@gmail.com>
2020-11-16 11:33:58 +0200

    Lab5.6

---
+++ Labaa5.1/Labaa5.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double h(const double x, const double y)
+{
+	return (x * y) / (1 + x * x * y * y);
+}
+
+int main()
+{
+	double s, t;
+
+	cout << "s = "; cin >> s;
+	cout << endl;
+	cout << "t = "; cin >> t;
+	cout << endl;
+
+	double l = (h(s * s, t * t) + h(s + t, 1) * h(s + t, 1))
+		/ (1 + h(s * t, 2) * h(s * t, 2));
+
+	cout << "l = " << l << endl;
+
+	return 0;
+}
